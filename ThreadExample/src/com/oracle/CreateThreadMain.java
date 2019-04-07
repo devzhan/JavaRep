c10ef40c37d16dd4256eaf16792d4a65f97491e9
Owen <zhxiuwen@163.com>
2019-04-07 23:23:14 +0800

    线程仓库初始化

---
+++ ThreadExample/src/com/oracle/CreateThreadMain.java
@@ -0,0 +1,8 @@
+package com.oracle;
+
+public class CreateThreadMain {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ ThreadExample/src/com/oracle/ThreadCreateDemo.java
@@ -0,0 +1,18 @@
+package com.oracle;
+
+public class ThreadCreateDemo {
+    public static void main(String[] args){
+        MyThread myThread = new MyThread() ;
+        myThread.start();
+
+    }
+
+
+}
+class MyThread  extends  Thread{
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("thread run");
+    }
+}
